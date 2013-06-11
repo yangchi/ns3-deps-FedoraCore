e513bda382c44c1592db06bc2fce556cf76d8239
Yang CHI <yangchi.cn@gmail.com>
2013-06-10 21:49:47 -0400

    added boost

--- install-deps.sh
+++ install-deps.sh
@@ -1,1 +1,1 @@
-sudo yum install gcc gcc-c++ python python-devel mercurial bzr gsl gsl-devel gtk2 gtk2-devel gdb valgrind graphviz ImageMagick python-sphinx dia texlive texlive-latex flex bison compat-gcc-34 tcpdump sqlite sqlite-devel libxml2 libxml2-devel uncrustify
+sudo yum install gcc gcc-c++ python python-devel mercurial bzr gsl gsl-devel gtk2 gtk2-devel gdb valgrind graphviz ImageMagick python-sphinx dia texlive texlive-latex flex bison compat-gcc-34 tcpdump sqlite sqlite-devel libxml2 libxml2-devel uncrustify boost boost-devel
